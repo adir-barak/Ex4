d3aa6ff852da0aaeec5501f6dfae959390f8c909
asher4cohen <asher4cohen@mail.huji.ac.il>
2024-03-06 23:40:56 +0200

    sun (without halo) done - section 4

--- src/pepse/main/ConstantsAsher.java
+++ src/pepse/main/ConstantsAsher.java
@@ -6,12 +6,17 @@ public class ConstantsAsher {
 
     public static final int SKY_LAYER = Layer.BACKGROUND;
     public static final int TERRAIN_LAYER = Layer.STATIC_OBJECTS;
-    public static final int NIGHT_LAYER = Layer.UI;
+    public static final int NIGHT_LAYER = Layer.FOREGROUND;
+    public static final int SUN_LAYER = Layer.BACKGROUND;
     public static final String GROUND_TAG = "ground";
     public static final String BLOCK_TAG = "block";
-    public static final String NIGHT_TAG = "NIGHT";
+    public static final String NIGHT_TAG = "night";
+    public static final String SUN_TAG = "sun";
     public static final int CYCLE_LENGTH = 30;//
     public static final Float NOON_OPACITY = 0f;
     public static final Float MIDNIGHT_OPACITY = 0.5f;
+    public static final float TERRAIN_START_PERCENT = 2/3f;
+    public static final float FACTOR_HALF = 0.5f;
+
 
 }
--- src/pepse/world/daynight/Sun.java
+++ src/pepse/world/daynight/Sun.java
@@ -1,4 +1,45 @@
 package pepse.world.daynight;
 
+import danogl.GameObject;
+import danogl.components.CoordinateSpace;
+import danogl.components.Transition;
+import danogl.gui.rendering.OvalRenderable;
+import danogl.gui.rendering.Renderable;
+import danogl.util.Vector2;
+import pepse.util.ColorSupplier;
+import pepse.world.Terrain;
+
+import java.awt.*;
+import java.util.function.Consumer;
+
+import static pepse.main.ConstantsAsher.*;
+
 public class Sun {
+
+
+    private static final Vector2 SIZE_OF_SUN = new Vector2(80,80);
+
+
+
+    public static GameObject create(Vector2 windowDimension, float cycleLength) {
+
+        Renderable renderable = new OvalRenderable(ColorSupplier.approximateColor(Color.YELLOW));
+        GameObject sun = new GameObject(windowDimension.mult(FACTOR_HALF),SIZE_OF_SUN, renderable);
+        sun.setCoordinateSpace(CoordinateSpace.CAMERA_COORDINATES);
+        sun.setTag(SUN_TAG);
+
+        Vector2 CYCLE_CENTER = new Vector2(windowDimension.x()*FACTOR_HALF, windowDimension.y()*TERRAIN_START_PERCENT);
+
+        Vector2 initialSunCenter = new Vector2(windowDimension.x()*FACTOR_HALF, windowDimension.y()*TERRAIN_START_PERCENT*FACTOR_HALF);
+
+        new Transition<Float>(sun,
+        (Float angle) -> sun.setCenter(initialSunCenter.subtract(CYCLE_CENTER).rotated(angle).add(CYCLE_CENTER)),
+        0f, 360f,
+        Transition.LINEAR_INTERPOLATOR_FLOAT, CYCLE_LENGTH,
+        Transition.TransitionType.TRANSITION_LOOP, null);
+
+
+
+        return sun;
+    }
 }
